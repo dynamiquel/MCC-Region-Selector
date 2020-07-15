70ee230c63bd0ef6647981ed3b797e48eba4dd26
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2020-07-15 17:52:17 +0100

    Add files via upload

---
+++ MCCRegionSelector/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCCRegionSelector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dictionary<string, Region> stringToRegion;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            stringToRegion = new Dictionary<string, Region>
+            {
+                ["Central US"] = Region.C_US,
+                ["North Central US"] = Region.NC_US,
+                ["East US"] = Region.E_US,
+                ["South Central US"] = Region.SC_US,
+                ["West US"] = Region.W_US,
+                ["North Europe"] = Region.N_EU,
+                ["West Europe"] = Region.W_EU,
+                ["East Asia"] = Region.E_AS,
+                ["South East Asia"] = Region.SE_AS,
+                ["East Japan"] = Region.E_JA,
+                ["West Japan"] = Region.W_JA,
+                ["East Australia"] = Region.E_AU,
+                ["South East Australia"] = Region.SE_AS
+            };
+
+            try
+            {
+                GetHostsFile();
+            }
+            catch
+            {
+                MessageBox.Show("An error occured. Ensure this app is being run in Admin mode.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private static string GetHostsFile()
+        {
+            return File.ReadAllText(@"C:\Windows\System32\drivers\etc\hosts");
+        }
+
+        private static void SaveHostsFile(string newHosts)
+        {
+            File.WriteAllText(@"C:\Windows\System32\drivers\etc\hosts", newHosts);
+        }
+
+        private void regionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            applyButton.IsEnabled = true;
+
+            var listBox = sender as ListBox;
+        }
+
+        private string ConfigureHostsFile(string hostsFile)
+        {
+            var hostsToAddSB = new StringBuilder("\n");
+
+            foreach (ListBoxItem item in regionsListBox.Items)
+            {
+                Region region = stringToRegion[item.Content as string];
+                var hosts = Regions.GetHostNames(region);
+
+                // Remove existing.
+                // Selected = to not include.
+                if (item.IsSelected)
+                {
+                    foreach (var host in hosts)
+                    {
+                        if (hostsFile.Contains(host))
+                            hostsFile = hostsFile.Replace($"\n{host}", string.Empty);
+                    }
+                }
+                // Add new.
+                else
+                {
+                    foreach (var host in hosts)
+                    {
+                        if (!hostsFile.Contains(host))
+                            hostsToAddSB.AppendLine(host);
+                    }
+                }
+            }
+
+            hostsFile += hostsToAddSB.ToString();
+
+            // Removes the extra empty lines.
+            hostsFile = hostsFile
+                .Replace("\r\n\r\n", string.Empty)
+                .Replace("\r\r", string.Empty)
+                .Replace("\n\n", string.Empty);
+
+            return hostsFile;
+        }
+
+        private void applyButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string hostsFile = GetHostsFile();
+                hostsFile = ConfigureHostsFile(hostsFile);
+                SaveHostsFile(hostsFile);
+
+                MessageBox.Show("Server regions adjusted.", "Success", MessageBoxButton.OK, MessageBoxImage.None);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not adjust the server regions. Ensure this app is being run in Admin mode.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void selectAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (ListBoxItem item in regionsListBox.Items)
+            {
+                item.IsSelected = true;
+            }
+        }
+
+        private void CheckBox_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+            Debug.WriteLine(cb.Parent);
+        }
+    }
+}
---
+++ MCCRegionSelector/Regions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCCRegionSelector
+{
+    public static class Regions
+    {
+        private static readonly Dictionary<Region, List<string>> regions;
+
+        static Regions()
+        {
+            regions = new Dictionary<Region, List<string>>
+            {
+                [Region.E_US] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.eastus.cloudapp.azure.com",
+                    "0.0.0.0 pfmsqosprod.eastus2.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-eus-9-18-2-0.cloudapp.net"
+                },
+                [Region.NC_US] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.northcentralus.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-ncus-9-18-2-0.cloudapp.net"
+                },
+                [Region.C_US] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.centralus.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-cus-9-18-2-0.cloudapp.net"
+                },
+                [Region.SC_US] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.southcentralus.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-scus-9-18-2-0.cloudapp.net"
+                },
+                [Region.W_US] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.westus.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-wus-9-18-2-0.cloudapp.net"
+                },
+                [Region.N_EU] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.northeurope.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-neu-9-18-2-0.cloudapp.net"
+                },
+                [Region.W_EU] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.westeurope.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-weu-9-18-2-0.cloudapp.net"
+                },
+                [Region.SE_AS] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.southeastasia.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-seas-9-18-2-0.cloudapp.net"
+                },
+                [Region.E_AS] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.eastasia.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-eas-9-18-2-0.cloudapp.net"
+                },
+                [Region.E_AU] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.australiaeast.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-aue-9-18-2-0.cloudapp.net"
+                },
+                [Region.SE_AU] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.australiasoutheast.cloudapp.azure.com",
+                    "0.0.0.0 pfmsqosprod2.australiasoutheast.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-ause-9-18-2-0.cloudapp.net"
+                },
+                [Region.W_JA] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.japanwest.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-jaw-9-18-2-0.cloudapp.net"
+                },
+                [Region.E_JA] = new List<string>()
+                {
+                    "0.0.0.0 pfmsqosprod.japaneast.cloudapp.azure.com",
+                    "0.0.0.0 xblcxplatqos-jae-9-18-2-0.cloudapp.net"
+                },
+            };
+        }
+
+        public static bool ContainsKey(Region region)
+        {
+            return regions.ContainsKey(region);
+        }
+
+        public static List<string> GetHostNames(Region region)
+        {
+            if (ContainsKey(region))
+                return regions[region];
+
+            return null;
+        }
+    }
+
+    public enum Region
+    {
+        E_US,
+        NC_US,
+        C_US,
+        SC_US,
+        W_US,
+        N_EU,
+        W_EU,
+        E_AS,
+        SE_AS,
+        W_JA,
+        E_JA,
+        E_AU,
+        SE_AU
+    }
+}
